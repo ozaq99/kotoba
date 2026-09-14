a50449fcdc73ab4f597100f330227dfc24ba138c
ozaq99 <muhammadnurrozaq99.com>
2026-09-14 14:26:20 +0700

    import { NextResponse } from 'next/server'; import type { NextRequest } from 'next/server';
    export function middleware(req: NextRequest) {
      const basicAuth = req.headers.get('authorization');
    
      if (basicAuth) {
        const authValue = basicAuth.split(' ')[1];
        const [user, pwd] = atob(authValue).split(':');
    
        if (user === process.env.SITE_USER && pwd === process.env.SITE_PASSWORD) {
          return NextResponse.next();
        }
      }
    
      return new NextResponse('Auth required', {
        status: 401,
        headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
      });
    }
    
    export const config = {
      matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
    };

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === process.env.SITE_USER && pwd === process.env.SITE_PASSWORD) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Auth required', {
+    status: 401,
+    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+  });
+}
+
+export const config = {
+  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+};
